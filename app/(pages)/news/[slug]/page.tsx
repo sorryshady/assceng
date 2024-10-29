9121404edb6ed8a6add4ce7f4bad3f0470fbf15a
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-10-29 17:35:07 +0530

    started working on individual news page

--- app/(pages)/news/[slug]/page.tsx
+++ app/(pages)/news/[slug]/page.tsx
@@ -1,6 +1,28 @@
 import Wrapper from "@/components/custom/wrapper";
+import { simpleNewsCard } from "@/lib/interface";
+import { client } from "@/lib/sanity";
 type Params = Promise<{ slug: string }>;
+
+async function getData(slug: string) {
+  const query = `*[_type == "news" && slug.current == "${slug}"] {
+  title,
+  image,
+  description,
+  content,
+  date,
+  "currentSlug": slug.current
+}[0]
+`;
+  const data = await client.fetch(query);
+  return data;
+}
 export default async function NewsPage({ params }: { params: Params }) {
   const { slug } = await params;
-  return <Wrapper>{slug} Page</Wrapper>;
+  const data: simpleNewsCard = await getData(slug);
+  return (
+    <Wrapper>
+      <h1 className="text-5xl font-bold text-center">News Details</h1>
+      <div>{data.title}</div>
+    </Wrapper>
+  );
 }
