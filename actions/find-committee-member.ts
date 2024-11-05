912ae17decea46089aa9473c5ccfe5f3fac02c25
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-11-06 00:46:52 +0530

    added verified check for committe members

--- actions/find-committee-member.ts
+++ actions/find-committee-member.ts
@@ -1,14 +1,17 @@
-'use server'
+"use server";
 
-import { db } from '@/db'
+import { db } from "@/db";
 
-export const findCommitteeMembers = async(committee: string) => {
-    if(committee === "state"){
-        const members = await db.user.findMany({ where: { committeeStatus: 'STATE' } })
-        return members
-    } else if(committee === "district"){
-        const members = await db.user.findMany({ where: { committeeStatus: 'DISTRICT' } })
-        return members
-    }
-    else return []
-}
+export const findCommitteeMembers = async (committee: string) => {
+  if (committee === "state") {
+    const members = await db.user.findMany({
+      where: { committeeStatus: "STATE", verifiedStatus: true },
+    });
+    return members;
+  } else if (committee === "district") {
+    const members = await db.user.findMany({
+      where: { committeeStatus: "DISTRICT", verifiedStatus: true },
+    });
+    return members;
+  } else return [];
+};
--- app/(pages)/login/[[...login]]/page.tsx
+++ app/(pages)/login/[[...login]]/page.tsx
@@ -1,13 +1,11 @@
 import LoginForm from "@/components/custom/login-form";
 import Wrapper from "@/components/custom/wrapper";
-import { SignIn } from "@clerk/nextjs";
 
 export default function Login() {
   return (
     <Wrapper>
       <h1 className="text-5xl font-bold text-center">Login</h1>
       <div className="flex items-center justify-center">
-        {/* <SignIn /> */}
         <LoginForm />
       </div>
     </Wrapper>
