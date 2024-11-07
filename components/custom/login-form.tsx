3e8309aafa017cafbef867fa526d1ebde6389b73
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-11-07 19:29:23 +0530

    finished login setup when clerkId and verified.

--- components/custom/login-form.tsx
+++ components/custom/login-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { LoginSchema } from "@/lib/login-schema";
+import { EmailSchema } from "@/lib/login-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -13,107 +13,48 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { useSignIn } from "@clerk/nextjs";
-import { useParams, useRouter } from "next/navigation";
 import { CardWrapper } from "./card-wrapper";
 import { useState } from "react";
-import ShowPassword from "./show-password";
 import { FormSuccess } from "./form-success";
 import { FormError } from "./form-error";
 import { findUserEmail } from "@/actions/find-user-email";
-import { toast } from "sonner";
+import ClerkSigninForm from "./clerk-signin-form";
 
 const LoginForm = () => {
-  const { isLoaded, signIn, setActive } = useSignIn();
-  const [showPassword, setShowPassword] = useState(false);
-  const [clerkReg, setClerkReg] = useState(false);
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [submitting, setSubmitting] = useState(false);
+  const [step, setStep] = useState(0);
+  const [email, setEmail] = useState("");
 
-  const router = useRouter();
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof EmailSchema>>({
+    resolver: zodResolver(EmailSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
-  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
-    if (!isLoaded) return;
-    console.log("calling onSubmit");
+  const onSubmit = async (values: z.infer<typeof EmailSchema>) => {
     setError("");
     setSuccess("");
     setSubmitting(true);
     const user = await findUserEmail(values.email);
     if (user) {
+      setEmail(values.email);
       if (user.verifiedStatus === "PENDING") {
         setError("Account has not yet been verified. Cannot login.");
       } else if (user.verifiedStatus === "VERIFIED") {
         if (user.clerkId) {
-          setClerkReg(true);
-          try {
-            const signInAttempt = await signIn.create({
-              identifier: values.email,
-              password: values.password,
-            });
-            if (signInAttempt.status === "complete") {
-              form.reset();
-              setSuccess("Login successful. Redirecting...");
-              const user = await findUserEmail(values.email);
-              toast.message("Account Incomplete");
-              await setActive({ session: signInAttempt.createdSessionId });
-              if (!user) {
-                router.push("/complete-account");
-              }
-            } else {
-              setError("Incorrect email or password");
-              console.error(JSON.stringify(signInAttempt, null, 2));
-            }
-          } catch (err: any) {
-            setError(err.errors[0].message);
-            console.error(JSON.stringify(err.errors[0].message, null, 2));
-          } finally {
-            setSubmitting(false);
-          }
+          setStep(1);
         } else {
-          setError("User not clerk registered.");
+          setStep(2);
         }
       } else if (user.verifiedStatus === "REJECTED") {
         setError("Your application has been rejected. Cannot login.");
       }
-      //   form.reset();
+    } else {
+      setError("Email does not exist in database.");
     }
-    toast(
-      <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-        <code className="text-white">{JSON.stringify(user, null, 2)}</code>
-      </pre>,
-    );
     setSubmitting(false);
-    // try {
-    //   const signInAttempt = await signIn.create({
-    //     identifier: values.email,
-    //     password: values.password,
-    //   });
-    //   if (signInAttempt.status === "complete") {
-    //     form.reset();
-    //     setSuccess("Login successful. Redirecting...");
-    //     const user = await findUserEmail(values.email);
-    //     toast.message("Account Incomplete");
-    //     await setActive({ session: signInAttempt.createdSessionId });
-    //     if (!user) {
-    //       router.push("/complete-account");
-    //     }
-    //   } else {
-    //     setError("Incorrect email or password");
-    //     console.error(JSON.stringify(signInAttempt, null, 2));
-    //   }
-    // } catch (err: any) {
-    //   setError(err.errors[0].message);
-    //   console.error(JSON.stringify(err.errors[0].message, null, 2));
-    // } finally {
-    //   setSubmitting(false);
-    // }
   };
   return (
     <CardWrapper
@@ -121,56 +62,36 @@ const LoginForm = () => {
       backButtonLabel="Don't have an account?"
       backButtonHref="/register"
     >
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 max-w-lg mx-auto"
-        >
-          <FormField
-            control={form.control}
-            disabled={submitting}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {clerkReg && (
+      {step === 0 && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 max-w-lg mx-auto"
+          >
             <FormField
               control={form.control}
               disabled={submitting}
-              name="password"
+              name="email"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Password</FormLabel>
+                  <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input
-                      placeholder="password"
-                      type={showPassword ? "text" : "password"}
-                      {...field}
-                    />
+                    <Input placeholder="Email" {...field} />
                   </FormControl>
-                  <ShowPassword
-                    showPassword={showPassword}
-                    setShowPassword={setShowPassword}
-                  />
                   <FormMessage />
                 </FormItem>
               )}
             />
-          )}
-          <FormSuccess message={success} />
-          <FormError message={error} />
-          <Button className="w-full" type="submit" disabled={submitting}>
-            Submit
-          </Button>
-        </form>
-      </Form>
+            <FormSuccess message={success} />
+            <FormError message={error} />
+            <Button className="w-full" type="submit" disabled={submitting}>
+              Submit
+            </Button>
+          </form>
+        </Form>
+      )}
+      {step === 1 && <ClerkSigninForm email={email} />}
+      {step === 2 && <div>Sign up flow</div>}
     </CardWrapper>
   );
 };
--- lib/login-schema.ts
+++ lib/login-schema.ts
@@ -13,3 +13,38 @@ export const LoginSchema = z.object({
     })
     .min(8, { message: "Password should be at least 8 characters" }),
 });
+
+export const EmailSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email address" }),
+});
+
+export const ClerkSigninSchema = z.object({
+  password: z
+    .string({
+      required_error: "Password is required",
+    }),
+});
+
+export const ClerkSignupSchema = z
+  .object({
+    password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .min(8, { message: "Password should be at least 8 characters" }),
+
+    confirmPassword: z
+      .string({
+        required_error: "Confirm password is required",
+      })
+      .min(8, { message: "Confirm password should be at least 8 characters" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
