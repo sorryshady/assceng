c868d6f6e57c62226dc773c5d72fb0fa7871e319
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-11-09 13:57:49 +0530

    desktop nav complete

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,24 +1,43 @@
 "use client";
-import Link from "next/link";
-import Image from "next/image";
-import { HoveredLink } from "../ui/navbar-menu";
-import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/nextjs";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import { HoveredLink } from "../ui/navbar-menu";
 import { MenuItem, menuItems } from "@/lib/navbar-data";
-import { SignedIn, useAuth, UserButton } from "@clerk/nextjs"; // Import Clerk's useAuth hook
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import SignOut from "./sign-out";
-import NavAnimation from "./nav-animation";
-import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 import { ChevronDown } from "lucide-react";
 import { Card } from "../ui/card";
+import NavAnimation from "./nav-animation";
 import { verifyClerkUser } from "@/actions/verify-clerk-user";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const Navbar = () => {
-  const { userId } = useAuth();
+  const { userId, isSignedIn } = useAuth();
   const pathname = usePathname();
-  const [active, setActive] = useState<string | null>(null);
+  const [user, setUser] = useState<{ name: string; admin: boolean }>({
+    name: "",
+    admin: false,
+  });
+
+  const fetchUser = async () => {
+    if (userId) {
+      const user = await verifyClerkUser(userId);
+      setUser({
+        name: user.name || "",
+        admin: user.userRole === "ADMIN",
+      });
+    }
+  };
+  useEffect(() => {
+    console.log(isSignedIn);
+    if (isSignedIn && userId) {
+      fetchUser();
+    } else {
+      setUser({ name: "", admin: false });
+    }
+  }, [isSignedIn, userId]);
 
   return (
     <NavAnimation>
@@ -40,7 +59,8 @@ const Navbar = () => {
                     <SubItems
                       subItem={item}
                       currentPath={pathname}
-                      userId={userId}
+                      userName={user.name}
+                      adminStatus={user.admin}
                     />
                   ) : (
                     <HoveredLink href={item.href}>{item.label}</HoveredLink>
@@ -51,68 +71,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      {/* <nav className="w-full border shadow-md fixed top-0 left-0 right-0 z-[20] bg-white">
-        <div className="max-w-7xl mx-auto hidden justify-between items-center p-5 lg:flex">
-          <Link href="/">
-            <Image
-              src="/assceng_logo.png"
-              alt="logo"
-              width={300}
-              height={300}
-            />
-          </Link>
-          <Menu setActive={setActive}>
-            {menuItems.map((item) =>
-              item.label === "Account" ? (
-                isSignedIn ? (
-                  <SignOut key={"user"} />
-                ) : (
-                  <MenuItem
-                    key={item.label}
-                    setActive={setActive}
-                    active={active}
-                    item={item.label}
-                  >
-                    <div className="flex flex-col space-y-4 text-sm">
-                      {item.subItems?.map((subItm) => (
-                        <HoveredLink key={subItm.label} href={subItm.href}>
-                          {subItm.label}
-                        </HoveredLink>
-                      ))}
-                    </div>
-                  </MenuItem>
-                )
-              ) : item.subItems ? (
-                <MenuItem
-                  key={item.label}
-                  setActive={setActive}
-                  active={active}
-                  item={item.label}
-                >
-                  <div className="flex flex-col space-y-4 text-sm">
-                    {item.subItems.map((subItm) => (
-                      <HoveredLink key={subItm.label} href={subItm.href}>
-                        {subItm.label}
-                      </HoveredLink>
-                    ))}
-                  </div>
-                </MenuItem>
-              ) : (
-                <Link
-                  href={item.href}
-                  className={cn(
-                    "hover:text-primary",
-                    pathname === item.href && "text-primary",
-                  )}
-                  key={item.label}
-                >
-                  {item.label}
-                </Link>
-              ),
-            )}
-          </Menu>
-        </div>
-      </nav> */}
     </NavAnimation>
   );
 };
@@ -122,26 +80,18 @@ export default Navbar;
 interface SubItemProps {
   subItem: MenuItem;
   currentPath: string;
-  userId: string | null | undefined;
+  userName: string;
+  adminStatus: boolean;
 }
-const SubItems = ({ subItem, currentPath, userId }: SubItemProps) => {
+
+const SubItems = ({
+  subItem,
+  currentPath,
+  userName,
+  adminStatus,
+}: SubItemProps) => {
   const [hover, setHover] = useState(false);
-  const [user, setUser] = useState({
-    name: "",
-    admin: false,
-  });
-  const fetchUser = async () => {
-    const user = await verifyClerkUser(userId);
-    setUser({
-      name: user.name,
-      admin: user.userRole === "ADMIN",
-    });
-  };
-  useEffect(() => {
-    if (userId) {
-      fetchUser();
-    }
-  }, []);
+
   return (
     <div
       className="hover:text-primary cursor-pointer relative"
@@ -149,7 +99,9 @@ const SubItems = ({ subItem, currentPath, userId }: SubItemProps) => {
       onMouseLeave={() => setHover(false)}
     >
       <div className="flex items-center gap-1">
-        <span>{user.name || subItem.label}</span>
+        <span>
+          {(subItem.label === "Account" && userName) || subItem.label}
+        </span>
         <span>
           <ChevronDown size={12} />
         </span>
@@ -158,11 +110,13 @@ const SubItems = ({ subItem, currentPath, userId }: SubItemProps) => {
         <div className="absolute top-full left-1/2 transform -translate-x-1/2">
           <div className="h-2 w-full" />
           <Card className="bg-white p-4 flex flex-col gap-4">
-            {/* Check if the label is 'Account' and render AccountLinks component */}
             {subItem.label === "Account" ? (
-              <AccountLinks /> // Assuming AccountLinks takes a user prop
+              <AccountLinks
+                userName={userName}
+                adminStatus={adminStatus}
+                currentPath={currentPath}
+              />
             ) : (
-              // Otherwise, render the default list of subitems
               subItem.subItems?.map((subItm) => (
                 <Link
                   href={subItm.href}
@@ -183,6 +137,73 @@ const SubItems = ({ subItem, currentPath, userId }: SubItemProps) => {
   );
 };
 
-const AccountLinks = () => {
-  return <div className="text-nowrap">Account Links</div>;
+interface AccountLinksProps {
+  userName: string;
+  adminStatus: boolean;
+  currentPath: string;
+}
+
+const AccountLinks = ({
+  userName,
+  adminStatus,
+  currentPath,
+}: AccountLinksProps) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(!!userName);
+  }, [userName]);
+
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        {!isLoggedIn ? (
+          <>
+            <Link
+              className={cn(
+                "text-nowrap hover:text-primary text-[15px]",
+                currentPath === "/login" && "text-primary",
+              )}
+              href={"/login"}
+            >
+              Login
+            </Link>
+            <Link
+              className={cn(
+                "text-nowrap hover:text-primary text-[15px]",
+                currentPath === "/register" && "text-primary",
+              )}
+              href={"/register"}
+            >
+              Register
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link
+              className={cn(
+                "text-nowrap hover:text-primary text-[15px]",
+                currentPath === "/my-account" && "text-primary",
+              )}
+              href={"/my-account"}
+            >
+              My Account
+            </Link>
+            {adminStatus && (
+              <Link
+                className={cn(
+                  "text-nowrap hover:text-primary text-[15px]",
+                  currentPath === "/admin" && "text-primary",
+                )}
+                href={"/admin"}
+              >
+                Admin Panel
+              </Link>
+            )}
+          </>
+        )}
+      </div>
+      {isLoggedIn && <SignOut />}
+    </>
+  );
 };
